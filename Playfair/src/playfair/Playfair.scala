c38e15d77b3638571179703d00b5115ab9280c07
Andrew Ho <andrewyc.ho@gmail.com>
2015-02-07 23:53:34 +0000

    completed code for the Playfair object, adding to runEncode(), runDecode() and validKey() methods

--- Playfair/src/playfair/Playfair.scala
+++ Playfair/src/playfair/Playfair.scala
@@ -22,22 +22,47 @@ object Playfair {
 	}
 
 	def runEncode = {
-		val key: String = readLine("Please enter the keyword: ")
+		val key: String = readLine("Please enter the key word: ")
 				//check that key is a letter
 				if(validKey(key)){
 					val code = new Coder(key)
 					val plainText = readLine("Please enter the name of a file to be encoded") 
-					val file = new File(plainText)
-					val plain = Source.fromFile(file).toString
-					code.encode(plain)
+					try{
+						val file = new File(plainText)
+						val plain = Source.fromFile(file).toString
+						println("The encoded version of your file is: \n")
+						println(code.encode(plain))
+					} catch {
+					  	case ex: FileNotFoundException => println("Missing file exception")
+					  	case ex: IOException => println("I/O exception")
+					}
+				} else {
+				  println("The key word you entered is invalid!  It must be comprised of only letters.")
 				}
 	}
 
 	def runDecode = {
-		???
+		val key: String = readLine("Please enter the key word: ")
+				//check that key is a letter
+				if(validKey(key)){
+					val code = new Coder(key)
+					val codedText = readLine("Please enter the name of a file to be decoded") 
+					try{
+						val file = new File(codedText)
+						val jibberish = Source.fromFile(file).toString
+						println("The decoded version of your file is: \n")
+						println(code.decode(jibberish))
+					} catch {
+					  	case ex: FileNotFoundException => println("Missing file exception")
+					  	case ex: IOException => println("I/O exception")
+					}
+				} else {
+				  println("The key word you entered is invalid!  It must be comprised of only letters.")
+				}
 	}
 
 	def validKey(key: String): Boolean = {
-		???
+		val existsNonLetter = key.filter(c => !c.isLetter)
+		existsNonLetter.length() == 0
 	}
 }
