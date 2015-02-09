757018b3219ac2f0b46a39cbc37a0c5565e9499c
Andrew Ho <andrewyc.ho@gmail.com>
2015-02-09 17:44:43 +0000

    Started writing the framework for the test suite methods I will need to write for the project

--- Playfair/src/playfairtests/CoderTest.scala
+++ Playfair/src/playfairtests/CoderTest.scala
@@ -6,4 +6,63 @@ import org.scalatest.BeforeAndAfter
 
 class CoderTest extends FunSuite with BeforeAndAfter {
 
+  // encode(String) method test suite
+  
+  test("encode method accepts mixed case string as input"){
+    ???
+  }
+  
+  test("encode method accepts string with spaces as input"){
+    ???
+  }
+  
+  test("encode method accepts string with newlines as input"){
+    ???
+  }
+  
+  test("encode method accepts string with punctuation marks as input"){
+    ???
+  }
+  
+  test("encode method should return a string with only lowercase letters"){
+    ???
+  }
+  
+  test("encode returns string with letters in blocks of 5, with a single space between blocks. Last block may contain fewer than 5 characters"){
+    ???
+  }
+  
+  test("encode method returns string with ten blocks per line - the last line may have fewer blocks"){
+    ???
+  }
+  
+  test("encode method returns string with no whitespace at the beginning or the end, and only a single space or a single newline between blocks"){
+	???
+  }
+  
+  test("encode method returns string with no punctuation"){
+	???
+  }
+  
+    // decode(String) method test suite
+  
+  test("decode method should return a string with only lowercase letters"){
+    ???
+  }
+  
+  test("decode returns string with letters in blocks of 5, with a single space between blocks. Last block may contain fewer than 5 characters"){
+    ???
+  }
+  
+  test("decode method returns string with ten blocks per line - the last line may have fewer blocks"){
+    ???
+  }
+  
+  test("decode method returns string with no whitespace at the beginning or the end, and only a single space or a single newline between blocks"){
+	???
+  }
+  
+  test("decode method returns string with no punctuation"){
+	???
+  }
 }
---
+++ Playfair/src/playfairtests/PlayfairTest.scala
@@ -0,0 +1,9 @@
+package playfairtests
+
+import playfair.Playfair
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+
+class PlayfairTest extends FunSuite with BeforeAndAfter {
+
+}
