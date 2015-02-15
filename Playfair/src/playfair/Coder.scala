6983087d6d1b4811f612a0b91f2f40a80cb176cb
Andrew Ho <andrewyc.ho@gmail.com>
2015-02-15 11:21:43 +0000

    completed code in the buildCodeBlock() method

--- Playfair/src/playfair/Coder.scala
+++ Playfair/src/playfair/Coder.scala
@@ -7,39 +7,44 @@ class Coder(val keyword: String) {
   
   private val cipherTable = buildCodeBlock()
  
-  private def buildCodeBlock() = {
+  private def buildCodeBlock(): Array[Array[Char]] = {
   	var codeBlock = new Array[Array[Char]](NumRows)   
+  	
     // concatenate the keyword with the alphabet
     val tableElements: List[Char] = keyword.toList ++ List('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+
     // replace all instances of the character j with i
+    def replaceJ(ch: Char):Char = {
+      if(ch == 'j') return 'i'
+      else return ch
+    }  
+  	
     val replaceJwithI = for(element <- tableElements) yield replaceJ(element)
+  	
     // remove any repeated characters in the list
     val distinctElements = replaceJwithI.distinct
     
-    val rowElements = getRowArrays
-    
-    def getRowArrays(): Array[char] = {
-      var result: Array[Char] = new Array[Char](NumRows)
+    //build the row elements to populate the block from distinctElements list
+    def getRowArrays(): Array[String] = {
+      var result: Array[String] = new Array[String](NumRows)
       for(r <- 0 until NumRows){
-        result(r) = distinctElements(r*NumCols+0) + distinctElements(r*NumCols+1) + distinctElements(r*NumCols+2) + distinctElements(r*NumCols+3) + distinctElements(r*NumCols+4)
+        result(r) = distinctElements(r*NumCols+0).toString ++ distinctElements(r*NumCols+1).toString ++ distinctElements(r*NumCols+2).toString ++ 
+        			distinctElements(r*NumCols+3).toString ++ distinctElements(r*NumCols+4).toString
       }
       return result
     }
     
+    val rowElements = getRowArrays
+        
     //initialise a 2D array
     for(r <- 0 until NumRows){
       codeBlock(r) = new Array[Char](NumCols)
     }
   	//populate the array
   	for(r <- 0 until NumRows){
-  	  codeBlock(r) = rowElements
+  	  codeBlock(r) = rowElements(r).toCharArray()
   	}
-    
-    def replaceJ(ch: Char):Char = {
-      if(ch == 'j') return 'i'
-      else return ch
-    }
-    
+    return codeBlock 
   }
     
 
@@ -47,7 +52,7 @@ class Coder(val keyword: String) {
   def encode(plainText: String): String = {
     val lowCasePT: String = plainText.toLowerCase()
     
-    
+    return ???
   }
   
   
