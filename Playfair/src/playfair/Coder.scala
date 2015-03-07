615ffca0325b6e01a00ecea9f9396c7dba1efdce
Andrew Ho <andrewyc.ho@gmail.com>
2015-03-07 09:55:05 +0000

    added a new internal getCoordinates() method to the encode() me

--- Playfair/src/playfair/Coder.scala
+++ Playfair/src/playfair/Coder.scala
@@ -58,7 +58,7 @@ class Coder(val keyword: String) {
     val onlyLetters: Array[Char] = PTchars.filter(_.isLetter)
     val letterList: List[Char] = onlyLetters.toList
     
-    // put an 'x' between double pairs or a 'q' between a double 'xx' 
+    // put an 'x' between double pairs or a 'q' between a double 'xx'.  If a final letter is needed to complete a pair, use ’z’.
     def removeDoublePairs(letters: List[Char]): List[Char] = {
       if(letters.size <= 0){
         return List()
@@ -73,21 +73,45 @@ class Coder(val keyword: String) {
     		      return List(letters(0),'x') ::: removeDoublePairs(letters.drop(1))
     		    }
     		  } else {
-    		    return List(letters(0),letters(1)) ::: removeDoublePairs(letters.drop(2))
+    		    return letters(0) :: removeDoublePairs(letters.drop(1))
     		  }
     	  }
       }
     }
  
     val noDoublePairs: List[Char] = removeDoublePairs(letterList)
-    val preprocessedTuples = noDoublePairs.grouped(2).toList
-
-    //map input char tuples to an output list of char tuples using the cipherTable to encode
-    def f(c: List[Char]) = {
-      //if(codeBlock.contains(c._1)){int xIndex = codeBlock.indexOf(c._1}
-    } 
+    val digraphs = noDoublePairs.grouped(2).toList
     
-    preprocessedTuples.map(x => f(x))
+    // find co-ordinates of Character in cipherTable: Array[Array[Char]]
+    def getCoordinates(c: Char): Tuple2[Int,Int] = {
+      var row: Int = -1
+      var col: Int = -1
+      for(i <- 0 until NumRows)
+        if(cipherTable(i).contains(c))
+          row = i
+      for(j <- 0 until NumCols)
+        if(cipherTable(row)(j) == c)
+          col = j
+        return Tuple2(row,col)
+    }
+     
+    for(pair <- digraphs){
+      val char1Coords = getCoordinates(pair(0))
+      val char2Coords = getCoordinates(pair(1))
+      
+      // get co-ordinates for the encoded char that the input char maps to
+      
+      // If the letters appear on the same row of your table, replace them with the letters to their immediate 
+      // right respectively (wrapping around to the left side of the row if a letter in the original pair was on the right side of the row).
+      
+      // If the letters appear on the same column of your table, replace them with the letters immediately 
+      // below respectively (wrapping around to the top side of the column if a letter in the original pair was on the bottom side of the column).
+      
+      // If the letters are not on the same row or column, replace them with the letters on the same row respectively but at the other pair of 
+      // corners of the rectangle defined by the original pair. The order is important – the first letter of the encrypted pair is the one that 
+      // lies on the same row as the first letter of the plaintext pair.
+    }
+    
     
     return ???
   }
