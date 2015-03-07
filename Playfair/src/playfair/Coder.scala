1ee3818b190227ec27a7b6d90da7230998a7bc92
Andrew Ho <andrewyc.ho@gmail.com>
2015-03-07 18:11:52 +0000

    Finished off code for encode method()

--- Playfair/src/playfair/Coder.scala
+++ Playfair/src/playfair/Coder.scala
@@ -52,6 +52,7 @@ class Coder(val keyword: String) {
 
 
   def encode(plainText: String): String = {
+    var result = List[Char]()
     //clean up plainText string
     val lowCasePT: String = plainText.toLowerCase()
     val PTchars: Array[Char] = lowCasePT.toCharArray()
@@ -82,7 +83,7 @@ class Coder(val keyword: String) {
     val noDoublePairs: List[Char] = removeDoublePairs(letterList)
     val digraphs = noDoublePairs.grouped(2).toList
     
-    // find co-ordinates of Character in cipherTable: Array[Array[Char]]
+    // find coordinates of Character in cipherTable: Array[Array[Char]]
     def getCoordinates(c: Char): Tuple2[Int,Int] = {
       var row: Int = -1
       var col: Int = -1
@@ -95,25 +96,53 @@ class Coder(val keyword: String) {
         return Tuple2(row,col)
     }
      
+    // get coordinates (rowNumber,columnNumber) for the encoded char that the input char maps to
+    
     for(pair <- digraphs){
-      val char1Coords = getCoordinates(pair(0))
-      val char2Coords = getCoordinates(pair(1))
-      
-      // get co-ordinates for the encoded char that the input char maps to
-      
+      val char1Coords: Tuple2[Int,Int] = getCoordinates(pair(0))
+      val char2Coords: Tuple2[Int,Int] = getCoordinates(pair(1))
+     
       // If the letters appear on the same row of your table, replace them with the letters to their immediate 
       // right respectively (wrapping around to the left side of the row if a letter in the original pair was on the right side of the row).
-      
+      if(char1Coords._1 == char2Coords._1){
+        if(char1Coords._2 >= NumCols -1){
+          result :::= List(cipherTable(char1Coords _1)(0))
+        } else {
+          result :::= List(cipherTable(char1Coords._1)(char1Coords._2 + 1))
+        }
+        if(char2Coords._2 >= NumCols -1){
+          result :::= List(cipherTable(char2Coords _1)(0))
+        } else {
+          result :::= List(cipherTable(char2Coords._1)(char2Coords._2 + 1))
+        }
+        
       // If the letters appear on the same column of your table, replace them with the letters immediately 
       // below respectively (wrapping around to the top side of the column if a letter in the original pair was on the bottom side of the column).
-      
+      } else if(char1Coords._2 == char2Coords._2){
+        if(char1Coords._1 >= NumRows -1){
+          result :::= List(cipherTable(0)(char1Coords _2))
+        } else {
+          result :::= List(cipherTable(char1Coords._1 + 1)(char1Coords._2))
+        }
+        if(char2Coords._1 >= NumRows -1){
+          result :::= List(cipherTable(0)(char2Coords _2))
+        } else {
+          result :::= List(cipherTable(char2Coords._1 + 1)(char2Coords._2))
+        }
+        
       // If the letters are not on the same row or column, replace them with the letters on the same row respectively but at the other pair of 
       // corners of the rectangle defined by the original pair. The order is important – the first letter of the encrypted pair is the one that 
-      // lies on the same row as the first letter of the plaintext pair.
+      // lies on the same row as the first letter of the plaintext pair.  
+      } else {
+        val encryptedFirstRow = char1Coords._1 
+        val encryptedFirstCol = char2Coords._2 
+        val encryptedSecondRow = char2Coords._1 
+        val encryptedSecondCol = char1Coords._2 
+        result :::= List(cipherTable(encryptedFirstRow)(encryptedFirstCol))
+        result :::= List(cipherTable(encryptedSecondRow)(encryptedSecondCol))
+      }
     }
-    
-    
-    return ???
+    return result.reverse.toString
   }
 
     
